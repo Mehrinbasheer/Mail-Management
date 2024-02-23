02649a78f1b0fef59773527a82a779b4bd7bdebf
Mehrinbasheer <mehrinbasheer01@gmail.com>
2024-02-23 13:29:42 +0530

    first commit

---
+++ src/main/java/com/example/mail/entity/User.java
@@ -0,0 +1,24 @@
+package com.example.mail.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
+    @Column(name = "email")
+    private String email;
+}
+
+
